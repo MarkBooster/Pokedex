7c6766e8e42d171992a9838dad55daaf93676b17
Mark Booster <markbooster@MacBook-Pro-van-Mark.local>
2016-05-20 23:09:26 +0200

    Finally found why JSON wasn't working

---
+++ Pods/Target Support Files/Pods-pokedex-by-markbooster/Pods-pokedex-by-markbooster-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_pokedex_by_markbooster : NSObject
+@end
+@implementation PodsDummy_Pods_pokedex_by_markbooster
+@end
---
+++ pokedex-by-markbooster/Constants.swift
@@ -0,0 +1,14 @@
+//
+//  Constants.swift
+//  pokedex-by-markbooster
+//
+//  Created by Mark Booster on 19-05-16.
+//  Copyright © 2016 Mark Booster. All rights reserved.
+//
+
+import Foundation
+
+let URL_BASE = "http://pokeapi.co"
+let URL_POKEMON = "/api/v1/pokemon/"
+
+typealias DownloadComplete = () -> Void
