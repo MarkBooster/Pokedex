425b1a7a45f1b6c5b3a2b23a187e46a5fd93f1e3
Mark Booster <markbooster@MacBook-Pro-van-Mark.local>
2016-05-21 00:03:38 +0200

    JSON Parsing went great, Let's go to the last lesson

--- pokedex-by-markbooster/Pokemon.swift
+++ pokedex-by-markbooster/Pokemon.swift
@@ -42,18 +42,65 @@ class Pokemon {
         let url = NSURL(string: _pokemonUrl)!
         Alamofire.request(.GET, url).responseJSON { response in
             let result = response.result
-            print(result.value.debugDescription)
             
+            if let dict = result.value as? Dictionary<String, AnyObject> {
+                
+                if let weight = dict["weight"] as? String {
+                    self._weight = weight
+                }
+                
+                if let height = dict["height"] as? String {
+                    self._height = height
+                }
+                
+                if let attack = dict["attack"] as? Int {
+                    self._attack = "\(attack)"
+                }
+                
+                if let defense = dict["defense"] as? Int {
+                    self._defense = "\(defense)"
+                }
+                
+                print(self._height)
+                print(self._attack)
+                print(self._defense)
+                print(self._weight)
+                
+                if let types = dict["types"] as? [Dictionary<String, String>] where types.count > 0 {
+                    if let name = types[0]["name"] {
+                        self._type = name.capitalizedString
+                        
+                    }
+                    
+                    if types.count > 1 {
+                        for var x = 1; x < types.count; x++ {
+                            if let name = types[x]["name"] {
+                                self._type! += "/\(name.capitalizedString)"
+                            }
+                        }
+                                
+                            }
+                            
+                } else {
+                    self._type = ""
+                }
+                print(self._type)
+                
+                
+            }
+                
+                
         }
+            
     }
 }
 
 
-        
 
         
 
-            
+        
+
 
         
 
